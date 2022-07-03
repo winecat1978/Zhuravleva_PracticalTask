169d46b282ec61e3a896849d7de46115eefdba38
Katherine_20 <mob1911494@gmail.com>
2022-07-03 17:42:35 +0300

    wrote and used a function that counts symbols and prints words in case of 3 and less symbols

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,3 +8,16 @@ string[] values = new string[] { s1, s2, s3, s4, s5 };
 string s_joined = string.Join(" ", values);
 
 Console.Write(s_joined + " -> ");
+
+void letters_checker (string [] values)
+{
+    for (int i = 0; i < values.Length; i++)
+    {
+        int longevity = values[i].Length;
+        if (longevity < 4)
+        {
+            Console.Write(values[i] + " ");
+        }
+    }
+}
+letters_checker(values);
