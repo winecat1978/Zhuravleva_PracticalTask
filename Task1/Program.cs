f44238e0c2bd8f02b2de489e2c5df794b563ee0b
Katherine_20 <mob1911494@gmail.com>
2022-07-03 17:41:12 +0300

    united objects into an array and printed it

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,8 @@ string s2 = "Dog";
 string s3 = "Country";
 string s4 = "Warior";
 string s5 = "he";
+
+string[] values = new string[] { s1, s2, s3, s4, s5 };
+string s_joined = string.Join(" ", values);
+
+Console.Write(s_joined + " -> ");
